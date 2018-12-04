4e4eca337ece487c992f84afa95eb614bb5cde2c
Pavol Dobrocka <pdb@whitestein.com>
2018-12-04 12:08:54 +0100

    Day 4

---
+++ day04/part2.js
@@ -0,0 +1,41 @@
+module.exports = input => [
+    ...input.split('\n')
+    .sort()
+    .reduce((res, line) => {
+      let match = /Guard #(\d+) begins shift/.exec(line);
+      if (match) {
+        res.current = match[1];
+      }
+
+      match = /\[\d+-\d+-\d+ 00:(\d+)\] falls asleep/.exec(line);
+
+      if (match) {
+        res.sleepingFrom = +match[1];
+      }
+
+      match = /\[\d+-\d+-\d+ 00:(\d+)\] wakes up/.exec(line);
+
+      if (match) {
+        const sleepingTo = +match[1];
+        const guard = res.guards.get(res.current) || {
+          time: 0,
+          minutes: [...new Array(60)].map(e => 0)
+        };
+
+        res.guards.set(res.current, guard);
+
+        for (let i = res.sleepingFrom; i < sleepingTo; i++) {
+          guard.minutes[i]++;
+          guard.time++;
+        }
+      }
+
+      return res;
+    }, {
+      current: null,
+      sleepingFrom: null,
+      guards: new Map()
+    }).guards.entries()
+  ]
+  .sort((a, b) => Math.max(...b[1].minutes) - Math.max(...a[1].minutes))
+  .map(e => e[0] * e[1].minutes.reduce((max, val, i, arr) => val > arr[max] ? i : max))[0]
